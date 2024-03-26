bba21967dc7181a5171f9dfe238e7efec6bf5d16
sara-dme <s.daoumy@gmail.com>
2024-03-26 04:14:24 -0700

    task 6

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,19 @@
+-- create a stored procedure addBonus
+-- that adds a new correction for a student
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS AddBonus.
+CREATE PROCEDURE AddBonus(
+	IN `user_id` INTEGER,
+	IN `project_name` VARCHAR(255),
+	IN `score` INTEGER
+)
+BEGIN
+	INSERT INTO projects (name)
+	SELECT project_name
+	WHERE project_name NOT IN(SELECT name FROM projects);
+
+	INSERT INTO corrections (user_id, project_id, score)
+	Values(user_id, (SELECT id from projects Where name=project_name), score);
+END $$
+DELIMITER ;$$
